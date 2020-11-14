a55de5d1b4293441b05a2730b8141bef857a5006
LinShi11 <50639335+LinShi11@users.noreply.github.com>
2020-11-13 21:14:20 -0700

    Update Homework4.java

--- src/test/java/edu/montana/csci/csci440/homework/Homework4.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework4.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,16 +35,16 @@ public class Homework4 extends DBTest {
 
         try(Connection connection = DB.connect()){
             connection.setAutoCommit(false);
-            PreparedStatement subtract = connection.prepareStatement("TODO");
-            subtract.setLong(1, 0);
-            subtract.setLong(2, 0);
+            PreparedStatement subtract = connection.prepareStatement("UPDATE tracks SET Milliseconds= (Milliseconds-?) WHERE TrackId = ?");
+            subtract.setLong(1,  10);
+            subtract.setLong(2, track1.getTrackId());
             subtract.execute();
 
-            PreparedStatement add = connection.prepareStatement("TODO");
-            subtract.setLong(1, 0);
-            subtract.setLong(2, 0);
-            subtract.execute();
-
+            PreparedStatement add = connection.prepareStatement("UPDATE tracks SET Milliseconds=(MILliseconds + ?) WHERE trackId=?");
+            add.setLong(1,  10);
+            add.setLong(2, 2);
+            add.execute();
+            connection.commit();
             // commit with the connection
         }
 
@@ -51,6 +53,7 @@ public class Homework4 extends DBTest {
         track2 = Track.find(2);
         assertEquals(track1.getMilliseconds(), track1InitialTime - 10);
         assertEquals(track2.getMilliseconds(), track2InitialTime + 10);
+
     }
 
     @Test
@@ -66,12 +69,25 @@ public class Homework4 extends DBTest {
     public void selectPopularTracksAndTheirAlbums() throws SQLException {
 
         // HINT: join to invoice items and do a group by/having to get the right answer
-        List<Map<String, Object>> tracks = executeSQL("");
+        List<Map<String, Object>> tracks = executeSQL("SELECT AlbumId, invoice_items.trackId, COUNT(DISTINCT CustomerId) as count FROM invoice_items\n" +
+                "    INNER JOIN tracks on tracks.TrackId = invoice_items.TrackId\n" +
+                "    INNER JOIN invoices on invoice_items.InvoiceId = invoices.InvoiceId\n" +
+                "GROUP BY tracks.TrackId\n" +
+                "HAVING 1 < count;\n" +
+                "\n" +
+                "SELECT * FROM temper\n" +
+                "GROUP BY temper.AlbumId;");
         assertEquals(256, tracks.size());
 
         // HINT: join to tracks and invoice items and do a group by/having to get the right answer
         //       note: you will need to use the DISTINCT operator to get the right result!
-        List<Map<String, Object>> albums = executeSQL("");
+
+        List<Map<String, Object>> albums = executeSQL("SELECT AlbumId FROM invoice_items\n" +
+                "    INNER JOIN tracks on tracks.TrackId = invoice_items.TrackId\n" +
+                "    INNER JOIN invoices on invoice_items.InvoiceId = invoices.InvoiceId\n" +
+                "GROUP BY tracks.TrackId\n" +
+                "HAVING 1 < COUNT(DISTINCT CustomerId)\n" +
+                "INTERSECT SELECT DISTINCT AlbumId FROM tracks;");
         assertEquals(166, albums.size());
     }
 
@@ -84,7 +100,13 @@ public class Homework4 extends DBTest {
      * */
     public void selectCustomersMeetingCriteria() throws SQLException {
         // HINT: join to invoice items and do a group by/having to get the right answer
-        List<Map<String, Object>> tracks = executeSQL("" );
+        List<Map<String, Object>> tracks = executeSQL("SELECT * FROM main.customers where\n" +
+                "SupportRepId = 3 AND CustomerId IN\n" +
+                "(SELECT invoices.CustomerId FROM invoices\n" +
+                "    WHERE InvoiceId IN (SELECT invoice_items.InvoiceId FROM invoice_items\n" +
+                "        WHERE TrackId IN (SELECT tracks.TrackId FROM tracks\n" +
+                "            WHERE AlbumId IN (select albums.AlbumId FROM albums\n" +
+                "                    ) AND tracks.GenreId = 1) ));" );
         assertEquals(21, tracks.size());
     }
 

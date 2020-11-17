b8ce2663c856e6a156d8d5288362acf1e7481b20
LinShi11 <50639335+LinShi11@users.noreply.github.com>
2020-11-17 08:40:33 -0700

    Update ResetDB.java

--- src/main/java/edu/montana/csci/csci440/util/ResetDB.java
+++ src/main/java/edu/montana/csci/csci440/util/ResetDB.java
@@ -1,9 +1,19 @@
 package edu.montana.csci.csci440.util;
 
+import edu.montana.csci.csci440.model.Track;
+import redis.clients.jedis.Jedis;
+
 import java.io.IOException;
 
 public class ResetDB {
     public static void main(String[] args) throws IOException {
         DB.reset();
+        try {
+            Jedis jedis = new Jedis();
+            jedis.del(Track.REDIS_CACHE_KEY);
+            System.out.println(jedis.get(Track.REDIS_CACHE_KEY));
+        } catch (Exception e) {
+            System.out.println("No redis found to reset");
+        }
     }
 }
